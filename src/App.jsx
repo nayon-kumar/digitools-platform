aca315357cdd480de59fcc6c71224bcdbc126596
nayon-kumar <nayon.cse55@gmail.com>
2026-03-29 21:28:02 +0600

    Project started with resources

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="btn btn-primary">Hello</h1>
+      <p className="text-4xl">World</p>
+    </>
+  );
+}
+
+export default App;
