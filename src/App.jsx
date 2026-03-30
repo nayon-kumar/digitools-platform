a8212522db5428323569d74067dc105fec9892d1
nayon-kumar <nayon.cse55@gmail.com>
2026-03-30 11:43:31 +0600

    Digital tools section design started

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Banner from "./components/Banner/Banner";
+import DigitalTools from "./components/DigitalTools/DigitalTools";
 import Navbar from "./components/Navbar/Navbar";
 import Performance from "./components/Performance/Performance";
 
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Banner />
       <Performance />
+      <DigitalTools />
     </>
   );
 }
---
+++ src/components/DigitalTools/DigitalTools.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DigitalTools = () => {
+  return (
+    <div>
+      <p>Digital Tools</p>
+    </div>
+  );
+};
+
+export default DigitalTools;
